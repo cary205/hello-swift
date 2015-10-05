39c7b1c6e7684e9275a59b0e47f863cea29030e7
cary205 <Cary@Carys-MacBook-Air.local>
2015-10-06 00:15:26 +0800

    Constants, Variables and Operators

---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//variables
+var x = 100
+var y = 1.1
+
+//constants
+let pi = 3
+
+//operators
+var z = x + 200
+z * pi
+
+
