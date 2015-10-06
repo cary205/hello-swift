cae9455727a5dc9ce20d4a432df9431679f78ce8
cary205 <Cary@Carys-MacBook-Air.local>
2015-10-06 21:51:39 +0800

    String

--- HelloSwift.playground/Contents.swift
+++ HelloSwift.playground/Contents.swift
@@ -1,4 +1,5 @@
 //: Playground - noun: a place where people can play
+// by Cary Wu
 
 import UIKit
 
@@ -14,5 +15,29 @@ let pi = 3
 //operators
 var z = x + 200
 z * pi
+
+
+//String===============================================================
+//concatenate String
+var hello = "Hello, "
+let swift = "Swift"
+var cool = "😎"
+//Hello, Swift. let's play 😎 it!
+var sentence = hello + swift + ". let's play " + cool + " it!"
+
+//String interpolation
+let version = 2.2
+//Hello, Swift 2.2. let's play 😎 it!
+//var sentence2 = hello + swift + version + ". let's play " + cool + " it!"   //ERROR!! version is Double
+var sentence2 = "\(hello) \(swift) \(version). let's play \(cool) it!"
+
+//String properties
+//var emptyString   //ERROR!!
+var emptyString = ""
+var oddString = "AbcdEFg"
+var loweroddString = "abckefg"
+emptyString.isEmpty
+oddString.uppercaseString
+oddString == loweroddString   //case sensitive
 
 
